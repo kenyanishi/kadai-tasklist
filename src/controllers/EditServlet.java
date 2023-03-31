e080ec9da88765ebd9cb3c093c90fcf058459163
kenya.nishi <knshi0821@gmail.com>
2023-03-31 22:40:19 +0900

    存在しないidに対する処理を追加

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -38,14 +38,17 @@ public class EditServlet extends HttpServlet {
 
         em.close();
 
-        // 情報とセッションIDをリクエストスコープに登録
+        // タスク情報とセッションIDをリクエストスコープに登録
         request.setAttribute("task", m);
         request.setAttribute("_token", request.getSession().getId());
 
         // IDをセッションスコープに登録
+        if(m != null) {
         request.getSession().setAttribute("task_id", m.getId());
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
         rd.forward(request, response);
+        }
     }
 }
+
